134cf2be08365feaa66e480990ada18263e5d2f9
pexcn <i@pexcn.me>
2020-01-05 20:49:35 +0800

    initial from power-button 6.2-0002.spk

---
+++ package/files/usr/bin/acpid.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+PATH_ACPID=/usr/bin/acpid
+
+case "$1" in
+
+start)
+    if [ -x "$PATH_ACPID" ] ; then
+            insmod /usr/lib/modules/evdev.ko
+            insmod /usr/lib/modules/button.ko
+            echo "start acpid"
+            $PATH_ACPID
+            logger -p daemon.info "$0 started acpid"
+    fi
+    ;;
+
+stop)
+    echo "stop acpid"
+    rmmod button
+    rmmod evdev
+    killall acpid > /dev/null 2>&1
+    logger -p daemon.info "$0 stopped acpid"
+    ;;
+
+*)
+    echo "usage: $0 { start | stop }" >&2
+    exit 1
+    ;;
+
+esac
+
